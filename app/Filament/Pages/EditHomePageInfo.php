3d9605ea353f638c1ab5a19f0a19573459824293
Oleksandr Moik <sashamoik2016@gmail.com>
2024-04-17 17:02:35 +0300

    refactor: Extract form sections to separate functions and use state path for sections

--- app/Filament/Pages/EditHomePageInfo.php
+++ app/Filament/Pages/EditHomePageInfo.php
@@ -19,8 +19,72 @@ class EditHomePageInfo extends EditPage
     {
         return $form
             ->schema([
-                Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.main_banner'))->columns()->schema([
-                    Forms\Components\FileUpload::make('hero_section.image.path')
+                $this->getMainBannerSection(),
+                $this->getPromoSection(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getMainBannerSection(): Forms\Components\Section
+    {
+        return Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.main_banner'))
+            ->columns()
+            ->statePath('hero_section')
+            ->schema([
+                Forms\Components\FileUpload::make('hero_section.image.path')
+                    ->required()
+                    ->columnSpan(1)
+                    ->hiddenLabel()
+                    ->disk('public')
+                    ->image()
+                    ->imageEditor()
+                    ->downloadable()
+                    ->openable()
+                    ->imagePreviewHeight('500'),
+
+                TranslatableTabs::make('Heading')
+                    ->columnSpan(1)
+                    ->makeNameUsing(fn ($name, $locale) => "{$name}.translations.{$locale}")
+                    ->localeTabSchema(fn (TranslatableTab $tab) => [
+                        Forms\Components\Grid::make()->schema([
+                            Forms\Components\TextInput::make($tab->makeName('textLeft'))
+                                ->maxLength(255)
+                                ->hiddenLabel()
+                                ->required(),
+                            Forms\Components\TextInput::make($tab->makeName('textRight'))
+                                ->maxLength(255)
+                                ->hiddenLabel()
+                                ->required(),
+                        ]),
+                        Forms\Components\TextInput::make($tab->makeName('quote'))
+                            ->maxLength(255)
+                            ->hiddenLabel()
+                            ->required(),
+                    ]),
+            ])->collapsed();
+    }
+
+    protected function getPromoSection(): Forms\Components\Section
+    {
+        return Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.promo_info'))
+            ->statePath('promo_section')
+            ->schema([
+                Forms\Components\Grid::make(4)->schema([
+                    TranslatableTabs::make('Heading')
+                        ->makeNameUsing(fn ($name, $locale) => "{$name}.translations.{$locale}")
+                        ->localeTabSchema(fn (TranslatableTab $tab) => [
+                            Forms\Components\TextInput::make($tab->makeName('title'))
+                                ->maxLength(255)
+                                ->hiddenLabel()
+                                ->required(),
+                            Forms\Components\RichEditor::make($tab->makeName('description'))
+                                ->maxLength(255)
+                                ->hiddenLabel()
+                                ->required(),
+                        ])
+                        ->columnSpan(3),
+
+                    Forms\Components\FileUpload::make('mainImage.path')
                         ->required()
                         ->columnSpan(1)
                         ->hiddenLabel()
@@ -29,71 +93,24 @@ class EditHomePageInfo extends EditPage
                         ->imageEditor()
                         ->downloadable()
                         ->openable()
-                        ->imagePreviewHeight('500'),
+                        ->imagePreviewHeight('300'),
+                ]),
 
-                    TranslatableTabs::make('Heading')
-                        ->columnSpan(1)
-                        ->localeTabSchema(fn (TranslatableTab $tab) => [
-                            Forms\Components\Grid::make()->schema([
-                                Forms\Components\TextInput::make("hero_section.textLeft.translations.{$tab->getLocale()}")
-                                    ->maxLength(255)
-                                    ->hiddenLabel()
-                                    ->required(),
-                                Forms\Components\TextInput::make("hero_section.textRight.translations.{$tab->getLocale()}")
-                                    ->maxLength(255)
-                                    ->hiddenLabel()
-                                    ->required(),
-                            ]),
-                            Forms\Components\TextInput::make("hero_section.quote.translations.{$tab->getLocale()}")
-                                ->maxLength(255)
-                                ->hiddenLabel()
-                                ->required(),
+                Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.slider'))->schema([
+                    Forms\Components\Repeater::make('slider')
+                        ->hiddenLabel()
+                        ->reorderable()
+                        ->reorderableWithDragAndDrop()
+                        ->grid(3)
+                        ->schema([
+                            Forms\Components\FileUpload::make('path')
+                                ->required()
+                                ->image()
+                                ->imageEditor()
+                                ->imagePreviewHeight('300'),
                         ]),
                 ])->collapsed(),
-
-                Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.promo_info'))->schema([
-                    Forms\Components\Grid::make(4)->schema([
-                        TranslatableTabs::make('Heading')
-                            ->localeTabSchema(fn (TranslatableTab $tab) => [
-                                Forms\Components\TextInput::make("promo_section.title.translations.{$tab->getLocale()}")
-                                    ->maxLength(255)
-                                    ->hiddenLabel()
-                                    ->required(),
-                                Forms\Components\RichEditor::make("promo_section.description.translations.{$tab->getLocale()}")
-                                    ->maxLength(255)
-                                    ->hiddenLabel()
-                                    ->required(),
-                            ])
-                            ->columnSpan(3),
-
-                        Forms\Components\FileUpload::make('promo_section.mainImage.path')
-                            ->required()
-                            ->columnSpan(1)
-                            ->hiddenLabel()
-                            ->disk('public')
-                            ->image()
-                            ->imageEditor()
-                            ->downloadable()
-                            ->openable()
-                            ->imagePreviewHeight('300'),
-                    ]),
-                    Forms\Components\Section::make(__('filament/pages/edit-home-page-info.sections.slider'))->schema([
-                        Forms\Components\Repeater::make('promo_section.slider')
-                            ->hiddenLabel()
-                            ->reorderable()
-                            ->reorderableWithDragAndDrop()
-                            ->grid(3)
-                            ->schema([
-                                Forms\Components\FileUpload::make('path')
-                                    ->required()
-                                    ->image()
-                                    ->imageEditor()
-                                    ->imagePreviewHeight('300'),
-                            ]),
-                    ])->collapsed(),
-                ])->collapsed(),
-            ])
-            ->statePath('data');
+            ])->collapsed();
     }
 
     protected function getRecordModel(): HomePageInfo
